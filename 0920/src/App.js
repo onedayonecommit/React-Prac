6f832dbd756d047e628622abbefe5b4353a255f2
onedayonecommit <rudghks0981@gmail.com>
2022-09-21 10:07:32 +0900

    페이지 만들기

--- 0920/src/App.js
+++ 0920/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
+import { Main, Login, Shop, Detail } from './page'
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/detail/:id/:num/:name' element={<Detail />} />
+      </Routes>
     </div>
   );
 }
